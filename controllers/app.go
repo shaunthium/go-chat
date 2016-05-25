8609cd2a88599c9ad117e9b26fd0dfe8537ab61b
Shaun Thium <shaun.thium@gmail.com>
2016-05-25 21:44:06 +0800

    Remove unnecessary password field for creating and joining rooms. Add comments and remove linter warnings.

--- controllers/app.go
+++ controllers/app.go
@@ -2,40 +2,47 @@ package controllers
 
 import "github.com/astaxie/beego"
 
+const (
+	httpMethodPOST = "POST"
+)
+
 var (
 	messages = make([]string, 20)
 )
 
+// MainController is the main controller for the app
 type MainController struct {
 	beego.Controller
 }
 
+// Get method for index page
 func (controller *MainController) Get() {
 	controller.TplName = "index.html"
 }
 
+// Create method for creating rooms
 func (controller *MainController) Create() {
 	controller.TplName = "create.html"
 
-	if controller.Ctx.Input.Method() == "POST" {
+	if controller.Ctx.Input.Method() == httpMethodPOST {
 		sessName := controller.GetString("name")
-		sessPass := controller.GetString("password")
-		controller.SetSession(sessName, sessPass)
+		controller.SetSession(sessName, "")
 		controller.Redirect("/room/"+sessName, 302)
 	}
 }
 
+// Join method for joining rooms
 func (controller *MainController) Join() {
 	controller.TplName = "join.html"
 
-	if controller.Ctx.Input.Method() == "POST" {
+	if controller.Ctx.Input.Method() == httpMethodPOST {
 		sessName := controller.GetString("name")
-		sessPass := controller.GetString("password")
-		controller.SetSession(sessName, sessPass)
+		controller.SetSession(sessName, "")
 		controller.Redirect("/room/"+sessName, 302)
 	}
 }
 
+// Room method representing a chat room
 func (controller *MainController) Room() {
 	controller.TplName = "room.html"
 
@@ -47,9 +54,10 @@ func (controller *MainController) Room() {
 	controller.Data["Pass"] = roomName
 }
 
+// Messages method, used as an API
 func (controller *MainController) Messages() {
 	controller.TplName = "room.html"
-	if controller.Ctx.Input.Method() == "POST" {
+	if controller.Ctx.Input.Method() == httpMethodPOST {
 		messages = append(messages, controller.GetString("message"))
 	}
 	if controller.Ctx.Input.Method() == "GET" {
